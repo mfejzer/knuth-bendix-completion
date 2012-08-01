3836276186657c0fc469f41c1c0b46eb9fd61f4c
Mikołaj Fejzer <mfejzer@gmail.com>
2012-08-01 20:52:11 +0200

    Changed Diagrams to be stanalone happstack server

--- Diagrams.hs
+++ Diagrams.hs
@@ -1,4 +1,7 @@
+module Main where 
 {-# LANGUAGE NoMonomorphismRestriction #-}
+import Control.Monad
+import Control.Monad.Trans (lift, liftIO)
 import Data.Tree
 import Diagrams.Prelude
 import Diagrams.TwoD.Layout.Tree
@@ -6,16 +9,32 @@ import Diagrams.Backend.Cairo
 import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.Backend.Cairo.Internal
 import Graphics.Rendering.Diagrams.Core
+import Happstack.Server (asContentType, dir, look, nullConf, ok, Response, serveFile, ServerPart, simpleHTTP, toResponse)
 
 t1 = Node 'A' [Node 'B' (map lf "CDE"), Node 'F' [Node 'G' (map lf "HIJ")]]
 
 lf :: Char -> Tree Char
 lf x = Node x []
 
-example =
-  renderTree ((<> circle 1 # fc white) . text . show)
-             (~~)
-             (symmLayout' with { slHSep = 4, slVSep = 4 } t1)
+simpleTree tree =
+	renderTree ((<> circle 1 # fc white) . text . show)
+		(~~)
+		(symmLayout' with { slHSep = 4, slVSep = 4 } tree)
 
-combos diagram = renderDia Cairo (CairoOptions "foo.png" (Width 250) PNG) diagram
-main = fst(combos example)
+translateName :: String -> String
+translateName name = name++".png"
+
+saveDiagram diagram name = renderDia Cairo (CairoOptions name (Width 250) PNG) diagram
+
+generate name tree = fst(saveDiagram (simpleTree tree) name)
+
+generatePart :: ServerPart Response
+generatePart =
+	do 
+		name <- look "name"
+		(liftIO $ generate (translateName name) t1)
+		serveFile (asContentType "image/png") (translateName name)
+
+notImplemented = ok $ toResponse $ "Not implemented yet"
+main = simpleHTTP nullConf $ msum [mzero, dir "add_axiom" $ notImplemented, dir "run_one_step" $ notImplemented ,dir "generate" $ generatePart, ok $ toResponse $ "Nothing here"]
+
